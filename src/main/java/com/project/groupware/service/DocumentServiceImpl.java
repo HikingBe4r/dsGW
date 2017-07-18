a0bb9d93e2ab52d8518a5b6cec87ef17511d6b32
kodica0309 <kodica0309@kodica0309-PC>
2017-07-18 10:15:18 +0900

    김영상 테스트

--- src/main/java/com/project/groupware/service/DocumentServiceImpl.java
+++ src/main/java/com/project/groupware/service/DocumentServiceImpl.java
@@ -1,15 +1,11 @@
 package com.project.groupware.service;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.project.groupware.domain.ApprovalHistoryVO;
 import com.project.groupware.domain.ApprovalLineVO;
