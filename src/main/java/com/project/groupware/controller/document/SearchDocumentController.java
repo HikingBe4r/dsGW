3167407b245774973fd4105eaa9990f1d779a358
kodica0309 <kodica0309@kodica0309-PC>
2017-07-20 16:23:13 +0900

    완료문서 검색 추가

--- src/main/java/com/project/groupware/controller/document/SearchDocumentController.java
+++ src/main/java/com/project/groupware/controller/document/SearchDocumentController.java
@@ -8,53 +8,51 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.project.groupware.domain.EmployeeVO;
 import com.project.groupware.service.DocumentService;
 
 @Controller
-public class ListDocumentController {
+public class SearchDocumentController {
 	
 	@Autowired
 	private DocumentService DocumentService;	
 	
-	@RequestMapping(value="/listDocument.do", method=RequestMethod.GET)
-	public ModelAndView listDocument(HttpServletRequest request/*, 
+	@RequestMapping(value="/searchDocument.do", method=RequestMethod.GET)
+	public String searchDocument(HttpServletRequest request, Model model,
 											@RequestParam(value="keyfield", required=false) String keyfield,
 											@RequestParam(value="keyword", required=false) String keyword,
 											@RequestParam(value="searchDay", required=false) String searchDay,
 											@RequestParam(value="startDay", required=false, defaultValue="all") String startDay,
-											@RequestParam(value="endDay", required=false, defaultValue="all") String endDay*/) {
+											@RequestParam(value="endDay", required=false, defaultValue="all") String endDay,
+											@RequestParam(value="myDocs", required=false) String myDocs,
+											@RequestParam(value="status", required=false) String status) {
 		
 		HttpSession session = request.getSession();
 		EmployeeVO employee = (EmployeeVO)session.getAttribute("employee");
 		
-		ModelAndView mv = new ModelAndView();
+		Map<String, Object> map = new HashMap<String, Object>();		
 		
-		Map<String, Object> map = new HashMap<String, Object>();
-		
-		int status = 3;
-		int myDocs = 2;
 		String employeeId = employee.getId();
 		
 		map.put("status", status);
 		map.put("myDocs", myDocs);
 		map.put("employeeId", employeeId);
-		/*map.put("keyfield", keyfield);
+		map.put("keyfield", keyfield);
 		map.put("keyword", keyword);
 		map.put("searchDay", searchDay);
 		map.put("startDay", startDay);
-		map.put("endDay", endDay);*/
+		map.put("endDay", endDay);
 		
-		mv.addObject("status", status);
-		mv.addObject("myDocs", myDocs);
-		mv.addObject("documentList", DocumentService.retrieveDocumentList(map));
-		mv.setViewName("approvalNav/document/listDocument");
+		model.addAttribute("status", status);
+		model.addAttribute("myDocs", myDocs);
+		model.addAttribute("documentList", DocumentService.retrieveDocumentList(map));
 		
-		return mv;
+		return "jsonView";
 		
 	}
 	
