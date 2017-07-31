35cd4e2f2a0500da6abce9a13e26cc5102d90175
kodica0303 <kodica0303@kodica0303-PC>
2017-07-31 10:56:19 +0900

    반려문서 재기안 오류수정

--- src/main/java/com/project/groupware/controller/document/RewriteDocumentController.java
+++ src/main/java/com/project/groupware/controller/document/RewriteDocumentController.java
@@ -2,20 +2,24 @@ package com.project.groupware.controller.document;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.project.groupware.domain.DocumentVO;
+
 @Controller
-public class rewriteDocumentController {
+public class RewriteDocumentController {
 	
-	@RequestMapping(value="/rewriteDocument.do", method=RequestMethod.GET)
+	@RequestMapping(value="/rewriteDocument.do", method=RequestMethod.POST)
 	public String rewriteDocumentForm(@RequestParam(value="subject") String subject,
 								  @RequestParam(value="content") String content,
 								  @RequestParam(value="endDate") String endDate,
 								  @RequestParam(value="formId") String formId,
 								  Model model) {
 		
+		System.out.println("content : " + content);
 		model.addAttribute("subject", subject);
 		model.addAttribute("content", content);
 		model.addAttribute("endDate", endDate.substring(0, 10));
@@ -24,5 +28,6 @@ public class rewriteDocumentController {
 	
 		return "approvalNav/document/rewriteDocument";
 	}
+	
 
 }
